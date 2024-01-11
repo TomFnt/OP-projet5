14185c1f31dbc1c8e9c9ea40e9ce7ba08df1e453
Tom Fontaine <tom.fnt9@gmail.com>
2024-01-11 10:36:34 +0100

    Revert "Implement quit command & first try catch approach in main.php"
    This reverts commit 1ae9d12396440bf0fc5d96571794da8e97a2907e.

--- exercice-1/main.php
+++ exercice-1/main.php
@@ -2,113 +2,115 @@
 include "Command.php";
 
 while (true) {
-    try{
-        $line = readline("Entrez votre commande (help, list, create, modify, delete, detail, quit) : ");
-        $pattern = '/^\S+|\S+(?=[,])|\d+/i';
-        preg_match_all($pattern,$line, $matches);
+    $line = readline("Entrez votre commande (help, list, create, modify, delete, detail) : ");
+    $pattern = '/^\S+|\S+(?=[,])|\d+/i';
+    preg_match_all($pattern,$line, $matches);
 
-        $matches= $matches[0];
-        $input = array_shift($matches);
 
-        switch ($input) {
-            case 'create':
-                if(count($matches)== 3){
-                    $name= array_shift($matches);
-                    $email= array_shift($matches);
-                    $phoneNumber=array_shift($matches);
-                    return Command::create($name, $email, $phoneNumber);
-                }
-                elseif (!empty($matches)){
-                    throw new Exception("\nAucun argument n'a été saisi. \n\nExemple pour créer un nouveau contact : create John Doe, johnd@gmail.com, 0312345678 \n\n");
-                }
-                else{
-                    throw new Exception("\nLe nombre d'arguments n'est pas valide. \n\nExemple pour créer un nouveau contact : create John Doe, johnd@gmail.com, 0312345678 \n\n");
-                }
+    $matches= $matches[0];
+    $input = array_shift($matches);
 
-                break;
-
-            case 'modify':
-            {
-                if (!empty($matches) && is_numeric($matches[0]))
-                {
-                    $id = (int)$matches[0];
-                    echo "\n Détail pour la commande n° $id : \n\n";
-                    echo Command::detail($id);
-                    $check= readline("Êtes-vous sûr de vouloir modifier ce contact ? (yes/no) : ");
-
-                    if($check == "yes" || $check=="y")
-                    {
-                        $contact = ContactManager::getContact($id);
-                        $defaultName=$contact->getName();
-                        $defaultEmail=$contact->getEmail();
-                        $defaultPhoneNumber=$contact->getPhoneNumber();
-
-                        $name = readline("Nom (valeur actuelle : $defaultName) :");
-                        $email = readline("Adresse mail (valeur actuelle : $defaultEmail) :");
-                        $phoneNumber= readline("Numéro de téléphone (valeur actuelle : $defaultPhoneNumber) :");
-
-                        Command::modify($defaultName, $defaultEmail, $defaultPhoneNumber, $name, $email, $phoneNumber, $id);
-                    }
-                    else{
-                        echo "\nAnnluation de la modification.\n\n";
-                    }
-
-                }
-                elseif (!empty($matches))
-                {
-                    throw new Exception("\n Vous n'avez pas saisi un ID valide. \n\nExemple pour a commande modify : modify 0 \n\n");
-                }
-                else {
-                    throw new Exception("\n La commande detail a été saisie sans ID. \n\nExemple pour a commande modify : modify 0 \n\n");
-                }
-                break;
+    switch ($input) {
+        case 'create':
+            if(count($matches)== 3){
+                $name= array_shift($matches);
+                $email= array_shift($matches);
+                $phoneNumber=array_shift($matches);
+                return Command::create($name, $email, $phoneNumber);
+            }
+            elseif (!empty($matches)){
+                echo "\nAucun argument n'a été saisi. \n\n";
+                echo "Exemple pour créer une nouveau contact : create John Doe, johnd@gmail.com, 0312345678 \n\n";
+            }
+            else{
+                echo "\nLe nombre d'argument n'est pas valide. \n\n";
+                echo "Exemple pour créer une nouveau contact : create John Doe, johnd@gmail.com, 0312345678 \n\n";
             }
 
-            case 'list':
-                echo " \n Liste des contact : \n \nid, name, email, phone number \n \n";
-                echo Command::list();
-                break;
+            break;
 
-            case 'detail':
-                if (!empty($matches) && is_numeric($matches[0]))
-                {
-                    $id = (int)$matches[0];
-                    echo "\n Détail pour la commande n° $id : \n\n";
-                    echo Command::detail($id);
-                }
-                elseif (!empty($matches))
-                {
-                    throw new Exception("\n Vous n'avez pas saisi un ID valide. \n\nExemple pour a commande detail : detail 0 \n\n");
-                }
-                else {
-                    throw new Exception("\n La commande detail a été saisie sans ID. \n\nExemple pour a commande detail: detail 0 \n\n");
-                }
-                break;
+        case 'modify':
+        {
+            if (!empty($matches) && is_numeric($matches[0]))
+            {
+                $id = (int)$matches[0];
+                echo "\n Détail pour la commande n° $id : \n\n";
+                echo Command::detail($id);
+                $check= readline("Êtes-vous sûr de vouloir modifier ce contact ? (yes/no) : ");
 
-            case 'delete':
-                if (!empty($matches) && is_numeric($matches[0]))
+                if($check = "yes")
                 {
-                    $id = (int)$matches[0];
-                    $check= readline("Êtes-vous sûr de vouloir supprimer le contact n°$id ? (yes/no) : ");
-                    if($check=="yes" || $check=="y")
-                    {
-                        Command::delete($id);
-                    }
-                    else
-                    {
-                        echo "\nAnnulation de la suppression du contact n°$id. \n\n";
-                    }
-                }
-                elseif (!empty($matches))
-                {
-                    throw new Exception("\n Vous n'avez pas saisi un ID valide. Exemple pour a commande delete : delete 0 \n\n");
-                }
-                else {
-                    throw new Exception("\n La commande delete a été saisie sans ID. Exemple pour a commande delete: delete 0 \n\n");
-                }
-                break;
+                    $contact = ContactManager::getContact($id);
+                    $defaultName=$contact->getName();
+                    $defaultEmail=$contact->getEmail();
+                    $defaultPhoneNumber=$contact->getPhoneNumber();
 
-            case 'help':
+                    $name = readline("Nom (valeur actuelle : $defaultName) :");
+                    $email = readline("Adresse mail (valeur actuelle : $defaultEmail) :");
+                    $phoneNumber= readline("Numéro de téléphone (valeur actuelle : $defaultPhoneNumber) :");
+
+                    Command::modify($defaultName, $defaultEmail, $defaultPhoneNumber, $name, $email, $phoneNumber, $id);
+                }
+                else{
+                    echo "Annluation de la modification.";
+                }
+
+            }
+            elseif (!empty($matches))
+            {
+                echo "\n Vous n'avez pas saisi un ID valide. \n\nExemple pour a commande modify : modify 0 \n\n";
+            }
+            else {
+                echo "\n La commande detail a été saisie sans ID. \n\nExemple pour a commande modify : modify 0 \n\n";
+            }
+            break;
+        }
+
+        case 'list':
+            echo " \n Liste des contact : \n \nid, name, email, phone number \n \n";
+            echo Command::list();
+            break;
+
+        case 'detail':
+            if (!empty($matches) && is_numeric($matches[0]))
+            {
+                $id = (int)$matches[0];
+                echo "\n Détail pour la commande n° $id : \n\n";
+                echo Command::detail($id);
+            }
+            elseif (!empty($matches))
+            {
+                echo "\n Vous n'avez pas saisi un ID valide. \n\nExemple pour a commande detail : detail 0 \n\n";
+            }
+            else {
+                echo "\n La commande detail a été saisie sans ID. \n\nExemple pour a commande detail: detail 0 \n\n";
+            }
+            break;
+
+        case 'delete':
+            if (!empty($matches) && is_numeric($matches[0]))
+            {
+                $id = (int)$matches[0];
+                $check= readline("Êtes-vous sûr de vouloir supprimer le contact n°$id ? (yes/no) : ");
+                if($check=="yes" || $check=="y")
+                {
+                   Command::delete($id);
+                }
+                else
+                {
+                echo "\nAnnulation de la suppression du contact n°$id. \n\n";
+                }
+            }
+            elseif (!empty($matches))
+            {
+                echo "\n Vous n'avez pas saisi un ID valide. Exemple pour a commande delete : delete 0 \n\n";
+            }
+            else {
+                echo "\n La commande delete a été saisie sans ID. Exemple pour a commande delete: delete 0 \n\n";
+            }
+            break;
+
+        case 'help':
             {
                 echo "\n help : affiche cette aide \n
                  \n list : affiche l'ensemble des contacts \n
@@ -117,27 +119,9 @@ while (true) {
                  \n delete [id] : supprime un contact \n
                  \n quit : quitter le programme \n";
             }
-            case 'quit':
-            {
-                $check= readline("Êtes-vous sûr de vouloir modifier ce contact ? (yes/no) : ");
-                if($check == "yes " || $check=="y")
-                {
-                    exit;
-                }
-                else
-                {
-                    echo "\n";
-                    break;
-                }
-            }
 
-            default:
-                throw new Exception(" \n Commande non reconnue. Utilisez la commande help pour afficher l'ensemble des commandes et leur fonctionnalité. \n\n");
-        }
+        default:
+            echo " \n Commande non reconnue. Utilisez la commande help pour afficher l'ensemble des commandes et leur fonctionnalité. \n\n";
+            break;
     }
-    catch (Exception $e)
-    {
-        echo "Erreur : " . $e->getMessage() . "\n";
-    }
-
 }
